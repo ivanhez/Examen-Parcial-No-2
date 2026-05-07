de5ce7837f47d47dcf82a60f2f64920cc320298a
Marlon Hernandez <ivanhez96@gmail.com>
2026-05-07 15:25:50 -0600

    feat: done

--- problemas.py
+++ problemas.py
@@ -1,51 +1,59 @@
-def hacer_sencillo(monto):
-    monedas = [25, 10, 5, 1]
-    resultado = {}
-    for moneda in monedas:
-        cantidad = monto // moneda
-        resultado[moneda] = cantidad
-        monto =  monto % moneda
-    return resultado, sum(resultado.values())
+# def hacer_sencillo(monto):
+#     monedas = [25, 10, 5, 1]
+#     resultado = {}
+#     for moneda in monedas:
+#         cantidad = monto // moneda
+#         resultado[moneda] = cantidad
+#         monto =  monto % moneda
+#     return resultado, sum(resultado.values())
 
-print("\nHacer sencillo")
-for monto in [293, 502, 99, 42, 1337]:
-    print(monto, hacer_sencillo(monto))
-
-
-def knapsack_fraccionado(W, items):
-    """
-    items: lista de tuplas (nombre, precio_total, unidades_disponibles)
-    """
-    ordenados = sorted(items, key=lambda x: x[1] / x[2], reverse=True)
-    restante = W
-    total = 0
-    solucion = []
-
-    for nombre, precio, unidades in ordenados:
-        if restante == 0:
-            break
-
-        densidad = precio / unidades
-        tomar = min(unidades, restante)
-        valor = tomar * densidad
-
-        solucion.append((nombre, tomar, valor, densidad))
-        total += valor
-        restante -= tomar
-
-    return total, solucion
+# print("\nHacer sencillo")
+# for monto in [293, 502, 99, 42, 1337]:
+#     print(monto, hacer_sencillo(monto))
 
 
-print("\nKNAPSACK FRACCIONADO")
-casos_knapsack = [
-    (50, [("item1", 60, 10), ("item2", 100, 20), ("item3", 120, 30)]),
-    (15, [("item1", 100, 10), ("item2", 60, 10), ("item3", 120, 30)]),
-    (60, [("item1", 100, 20), ("item2", 60, 10), ("item3", 120, 30), ("item4", 75, 15)]),
-    (37, [("item1", 64, 52), ("item2", 56, 11), ("item3", 92, 26), ("item4", 111, 43)]),
-]
 
-for W, items in casos_knapsack:
-    print(W, knapsack_fraccionado(W, items))
+
+
+
+# def knapsack_fraccionado(W, items):
+#     """
+#     items: lista de tuplas (nombre, precio_total, unidades_disponibles)
+#     """
+#     ordenados = sorted(items, key=lambda x: x[1] / x[2], reverse=True)
+#     restante = W
+#     total = 0
+#     solucion = []
+
+#     for nombre, precio, unidades in ordenados:
+#         if restante == 0:
+#             break
+
+#         densidad = precio / unidades
+#         tomar = min(unidades, restante)
+#         valor = tomar * densidad
+
+#         solucion.append((nombre, tomar, valor, densidad))
+#         total += valor
+#         restante -= tomar
+
+#     return total, solucion
+
+
+# print("\nKNAPSACK FRACCIONADO")
+# casos_knapsack = [
+#     (50, [("item1", 60, 10), ("item2", 100, 20), ("item3", 120, 30)]),
+#     (15, [("item1", 100, 10), ("item2", 60, 10), ("item3", 120, 30)]),
+#     (60, [("item1", 100, 20), ("item2", 60, 10), ("item3", 120, 30), ("item4", 75, 15)]),
+#     (37, [("item1", 64, 52), ("item2", 56, 11), ("item3", 92, 26), ("item4", 111, 43)]),
+# ]
+
+# for W, items in casos_knapsack:
+#     print(W, knapsack_fraccionado(W, items))
+
+
+
+
 
 
 VECINOS = {
